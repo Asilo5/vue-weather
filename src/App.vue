f82a6983906b5ba13e51ecec7f1cc765aaaee964
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-05 21:48:22 -0600

    Add search bar to be able to search for weather

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <section id="app">
-    
+    <main>
+      <form class="search-box">
+          <input type="text" 
+                 class="search-bar" 
+                 placeholder="Search..." />
+      </form>
+    </main>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      api_key: 'c117c79223f634582cd813b812d3e14b'
+    }
+  }
 }
 </script>
 
