8cd0523bcb0afbfe3667c944130be1b95f45af77
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-05-05 22:54:49 -0600

    Add style to the search-bar

--- src/App.vue
+++ src/App.vue
@@ -43,4 +43,24 @@ export default {
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
   }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline-color: rgb(141, 57, 189);
+    background: none;
+    background-color: rgba(255, 255, 255, 0.15);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+  }
 </style>
